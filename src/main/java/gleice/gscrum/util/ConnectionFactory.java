88803b4ed9c890a1ef9543fdd4798bb35ca6f69a
jopss <jopss.sossoloti@gmail.com>
2017-04-03 15:16:41 -0300

    melhorias no DAO e configs

---
+++ src/main/java/gleice/gscrum/util/ConnectionFactory.java
@@ -0,0 +1,20 @@
+package gleice.gscrum.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+
+        public static Connection getConnection() {
+                try {
+//                        DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+//                        return DriverManager.getConnection("jdbc:mysql://localhost/gscrum", "root", "");
+                        DriverManager.registerDriver(new org.postgresql.Driver());
+                        return DriverManager.getConnection("jdbc:postgresql://localhost/gscrum", "postgres", "postgres");
+                } catch (SQLException e) {
+                        throw new RuntimeException(e);
+                }
+        }
+
+}
